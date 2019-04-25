d03185f621ea682e56c4ed23d17e612ad81589fd
Angelina Stilphen <astilphen000@west-mec.org>
2019-04-24 18:04:03 -0700

    Final Commit
    Ready for review

--- dl_expense.js
+++ dl_expense.js
@@ -31,17 +31,22 @@
       
 */
 
+// adds event listener to load the functions onto the web page
 window.addEventListener("load", function() {
-      // 
+      // macthes all input elements in the travlExp table that belong to the sum class
       var changingCells = document.querySelectorAll('input[class="sum"]');
+      // the for loop runs every item in the changingCells collection and adds an onchange event handlerthat runs the calcExp function
       for (var i = 0; i < changingCells.length; i++) {
             changingCells[i].onchange = calcExp;
       }
+      // submits summary once the button is clicked and runs you to the validateSummary function
       document.getElementById("submitButton").onclick = validateSummary;
 }); 
 
 function validateSummary() {
+      // runs the validateSummary function and grabs the element summary by id
       var validateSummary = document.getElementById("summary");
+      // if the validation state of the summary field value is missing it displays the message below 
       if (validateSummary.validity.valueMissing) {
             validateSummary.setCustomValidity("You must include a summary of thr trip in your report");
       } else {
@@ -50,9 +55,39 @@ function validateSummary() {
 }
 
 function calcClass(sumClass) {
-      var sumFields = document.getElementsByClassName(sumClass);
-      var subTotal = document.getElementById()
+      // the sumFields variable grabs the class name sumClass from the document 
+      var sumfiels = document.getElementsByClassName(sumClass);
+      // variable subTotal is used to keep a running total of the total values in the input elements in the sumFields object collection 
+      var subTotal = 0;
+      //  createf for loop to loop through the items in the sumFields object collection 
+      for (var i = 0; i < sumfiels.length; i++) {
+            // created a variable named itemValue to be equal to the numeric value of the current input element in the sumFields array.
+            var itemValue = parseFloat(sumfiels[i].value);
+            //  the itemValue is a numeric value and it needs to be added to itemValue
+            if (!isNaN(itemValue)) {
+
+            } else {
+                  
+            }
+            return sumTotal; 
+      }
+} 
+
+// the purpose of this function is to calculate the the row and column totals from the travelExp table 
+function calcExp() {
+      // the expTable variable grabs all the table rows in the body of the tr elements 
+      var expTable = document.querySelectorAll(tr);
+      // loops through the the rows in the expTable collection 
+      for (var i = 0; i < expTotal.length; i++) {
+            // grabs the id subTotal value in the index and returned by the calcClass function using the parameters date and index, then added 2 decimals
+            document.getElementById(subTotal + i).value = formatNumber(calcClass("date" + i), 2);
+      } 
+      document.getElementById("transTotal").value = formatNumber(calcClass("trans"), 2);
+      document.getElementById("lodgeTotal").value = formatNumber(calcClass("lodge" + i), 2);
+      document.getElementById("mealTotal").value = formatNumber(calcClass("meal" + i), 2);
+      document.getElementById("otherTotal").value = formatNumber(calcClass("other" + i), 2);
 }
+
 
 
 function formatNumber(val, decimals) {
